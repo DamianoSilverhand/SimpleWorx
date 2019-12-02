d5226ea958e378748eaeb52baeb593947de25a09
Damiano Chintala <damianochintala@gmail.com>
2019-12-02 12:09:41 +0200

    Changing api Json into Javascript object

--- node-weather/index.js
+++ node-weather/index.js
@@ -4,6 +4,8 @@ let apiKey = '4176b01f3b069bf25ca0ad38079389b6';
 let city = 'lusaka';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
+let weather = JSON.parse(body)
+
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
