bbba9cf656bc61cda0ff96b0b70cf3c75a72e9da
Damiano Chintala <damianochintala@gmail.com>
2019-12-02 12:26:32 +0200

    Changed body to display message string

--- node-weather/index.js
+++ node-weather/index.js
@@ -4,16 +4,14 @@ let apiKey = '4176b01f3b069bf25ca0ad38079389b6';
 let city = 'lusaka';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
-let weather = JSON.parse(body)
-
-let message = `It's ${weather.main.temp} degrees in
-               ${weather.name}!`;
-console.log(message);
 
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    console.log('body:', body);
-  }
+let weather = JSON.parse(body);
+let message = `It's ${weather.main.temp} degrees in
+               ${weather.name}!`;
+console.log(message);
+      }
 });
