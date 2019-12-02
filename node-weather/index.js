d495ab58beaef0f67ffe5f63580faca63171524d
Damiano Chintala <damianochintala@gmail.com>
2019-12-02 12:04:21 +0200

    Adding api request to open weather

---
+++ node-weather/index.js
@@ -0,0 +1,13 @@
+const request = require('request');
+
+let apiKey = '4176b01f3b069bf25ca0ad38079389b6';
+let city = 'lusaka';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    console.log('body:', body);
+  }
+});
